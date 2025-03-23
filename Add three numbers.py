ba8e0f768465c7c1c3fdb42ec0f00e5618f89530
Sam29W <samithshivakumar1@gmail.com>
2025-03-23 19:23:29 +0530

    Add files via upload

---
+++ Add three numbers.py
@@ -0,0 +1,6 @@
+#python program to add three numbers 
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+c = int(input("Enter the third number: "))
+sum = a+b+c
+print("Sum is",sum)
